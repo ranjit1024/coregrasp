f37ebe92b02a374a9d1c076dbfd2847d2017e105
ranjit1024 <ranjitdas2048@gmail.com>
2026-07-01 19:22:30 +0530

    done:done with auth gogle

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
-
+import {authClient} from "@/lib/auth-client"
 
 export default function AuthPage() {
   const [isLogin, setIsLogin] = useState(true)
@@ -64,8 +64,13 @@ export default function AuthPage() {
               </div>
 
               {/* OAuth Buttons */}
-              <div className="flex hover:cursor-pointer flex-col gap-3 mb-6">
-                <button className="flex items-center hover:cursor-pointer justify-center gap-3 w-full bg-white/5 hover:bg-white/10 border border-white/10 text-white text-[14px] font-medium py-2.5 rounded-lg transition-colors">
+              <div  className="flex hover:cursor-pointer flex-col gap-3 mb-6">
+                <button onClick={()=>
+                  authClient.signIn.social({
+                    provider:"google",
+                    callbackURL: "/home/dashboard"
+                  })
+                } className="flex items-center hover:cursor-pointer justify-center gap-3 w-full bg-white/5 hover:bg-white/10 border border-white/10 text-white text-[14px] font-medium py-2.5 rounded-lg transition-colors">
                   <svg className="w-4 h-4" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
                     <path fill="currentColor" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
