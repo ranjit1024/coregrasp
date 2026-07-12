cb13c22fb6fd886dfe9fe55aa7c7f19c2872d04a
ranjit1024 <ranjitdas2048@gmail.com>
2026-07-12 09:58:49 +0530

    update:update policy FE

--- apps/web/app/home/policies/[policy]/page.tsx
+++ apps/web/app/home/policies/[policy]/page.tsx
@@ -1,3 +1,22 @@
-export default function PolicyDetails(){
-    return <div>fsdf</div>
+"use client";
+
+import { use, useEffect, useState } from "react";
+
+export default function PolicyDetails({
+    params,
+}: {
+    params: Promise<{ policy: string }>;
+}) {
+    const { policy } = use(params); 
+    console.log(policy)
+    const [data, setData] = useState<any>(null);
+    const ans = fetch(`https://api.ranjitdas2048.workers.dev/ans/${policy}`);
+    console.log(ans)
+    if (!data) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h1>Policy {data.answer?.id}</h1>
+        </div>
+    );
 }
