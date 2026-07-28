ab5666d96a398829334f098159ad6e76fb441696
ranjit1024 <ranjitdas2048@gmail.com>
2026-07-28 22:47:16 +0530

    add : added dashborad api

---
+++ apps/web/lib/dashboard.ts
@@ -0,0 +1,16 @@
+"use server"
+import prisma from "@/lib/prisma";
+import { auth } from "./auth";
+import { headers } from "next/headers";
+export async function Dashboard_data(){
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+    if(!session){
+        return null;
+    }
+    const email = session.user.email;
+
+    // Top bar
+    
+} 
